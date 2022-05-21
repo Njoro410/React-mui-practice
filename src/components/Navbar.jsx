43d2cdd12d9f370f3e9fdc7bb5393d35ba21b99e
njoro410 <brian.njoroge@student.moringaschool.com>
2022-05-21 16:02:16 +0300

    added a search tab

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React from "react";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import { Typography, styled, Box, InputBase, Badge } from "@mui/material";
+import PublicIcon from "@mui/icons-material/Public";
+import { Mail } from "@mui/icons-material";
+
+const StyledToolbar = styled(Toolbar)({
+  display: "flex",
+  justifyContent: "space-between",
+});
+
+const Search = styled("div")(({ theme }) => ({
+  backgroundColor: "white",
+  padding: "0 10px",
+  borderRadius: theme.shape.borderRadius,
+  width: "40%",
+}));
+
+const Icons = styled(Box)(({ theme }) => ({
+  backgroundColor: "white",
+}));
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <AppBar position="sticky">
+      <StyledToolbar>
+        <Typography variant="h6" sx={{ display: { xs: "none", sm: "block" } }}>
+          BORU
+        </Typography>
+        <PublicIcon sx={{ display: { xs: "block", sm: "none" } }} />
+        <Search>
+          <InputBase placeholder="search..." />
+        </Search>
+        <Icons>
+          <Badge badgeContent={4} color="primary">
+            <Mail color="action" />
+          </Badge>
+        </Icons>
+      </StyledToolbar>
+    </AppBar>
+  );
+};
 
-export default Navbar
+export default Navbar;
