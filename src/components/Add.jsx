fa512d8fcfeb23494b30dfbc2e1590011a36bb4b
njoro410 <brian.njoroge@student.moringaschool.com>
2022-05-27 14:14:06 +0300

    added a button witha a tooltip

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -17,14 +17,7 @@ const Add = () => {
           <AddIcon />
         </Fab>
       </Tooltip>
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box>Hello</Box>
-      </Modal>
+
     </>
   );
 };
